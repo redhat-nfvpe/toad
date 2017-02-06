dad880a588c080cfc50d323196603a68f18192f0
Yolanda Robla <yroblamo@redhat.com>
2017-02-06 21:34:19 +0100

    Latest fixes on create repo script

--- scripts/create_repo.sh
+++ scripts/create_repo.sh
@@ -1,17 +1,18 @@
 ADDRESS=$1
+REPONAME=$2
 HOSTNAME=$(hostname -f)
-REPOPATH="/var/ftp/pub/osp_repo"
+REPOPATH="/var/ftp/pub/"
 REPOFILE="${REPOPATH}/osp.repo"
 
 mkdir -p $REPOPATH
-cp -R $ADDRESS /var/ftp/pub/
+cp -R "${ADDRESS}/${REPONAME}/" ${REPOPATH}
 rm $REPOFILE 2> /dev/null
 touch $REPOFILE
 
-for DIR in `find /var/ftp/pub/osp_repo -maxdepth 1 -mindepth 1 -type d`; do
+for DIR in `find ${REPOPATH}${REPO_NAME}/* -maxdepth 1 -mindepth 1 -type d`; do
     echo -e "[`basename $DIR`]" >> $REPOFILE
     echo -e "name=`basename $DIR`" >> $REPOFILE
-    echo -e "baseurl=ftp://$HOSTNAME/pub/osp_repo/`basename $DIR`/" >> $REPOFILE
+    echo -e "baseurl=ftp://$HOSTNAME/pub/${REPONAME}/`basename $DIR`/" >> $REPOFILE
     echo -e "enabled=1" >> $REPOFILE
     echo -e "gpgcheck=1" >> $REPOFILE
     echo -e "gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release" >> $REPOFILE
