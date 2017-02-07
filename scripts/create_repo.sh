40f27a55a5e07dbc59aca56ad12d5ab8a9bc3997
Yolanda Robla <yroblamo@redhat.com>
2017-02-07 08:31:57 +0100

    Use public ip, not hostname, for the repos
    It was failing under environments where dns is not
    setup.

--- scripts/create_repo.sh
+++ scripts/create_repo.sh
@@ -1,6 +1,6 @@
 ADDRESS=$1
 REPONAME=$2
-HOSTNAME=$(hostname -f)
+PUBLIC_ADDRESS=$(hostname --ip-address)
 REPOPATH="/var/ftp/pub/"
 REPOFILE="${REPOPATH}/${REPONAME}/osp.repo"
 
@@ -9,13 +9,16 @@ cp -R "${ADDRESS}/${REPONAME}/" ${REPOPATH}
 rm $REPOFILE 2> /dev/null
 touch $REPOFILE
 
+echo "path is $REPOPATH"
+echo "name is $REPONAME"
 for DIR in `find ${REPOPATH}${REPO_NAME}/* -maxdepth 1 -mindepth 1 -type d`; do
+    echo "directory"
+    echo $DIR
     echo -e "[`basename $DIR`]" >> $REPOFILE
     echo -e "name=`basename $DIR`" >> $REPOFILE
-    echo -e "baseurl=ftp://$HOSTNAME/pub/${REPONAME}/`basename $DIR`/" >> $REPOFILE
+    echo -e "baseurl=ftp://${PUBLIC_ADDRESS}/pub/${REPONAME}/`basename $DIR`/" >> $REPOFILE
     echo -e "enabled=1" >> $REPOFILE
     echo -e "gpgcheck=1" >> $REPOFILE
     echo -e "gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release" >> $REPOFILE
     echo -e "\n" >> $REPOFILE
 done;
-
